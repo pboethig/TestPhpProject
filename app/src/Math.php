b5f70299c1116d31fc901a54276897cc27f66951
pboethig <pboethig@gmail.com>
2016-09-24 20:27:51 +0200

    move nexuscredential to builproperties and refactore xml ant tasks

--- app/src/Math.php
+++ app/src/Math.php
@@ -1,14 +1,11 @@
 <?php
-
 namespace TestPhpProject;
-
 Class Math
 {
     public function __construct()
     {
         $this->_test = '1';
     }
-
     public function add($x, $y)
     {
         return $x+$y;
